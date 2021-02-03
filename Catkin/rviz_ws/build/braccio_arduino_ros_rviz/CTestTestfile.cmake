fa8f7c31db344ed0e8032670e804b2f68d79ebb8
LCN-Ubuntu <18252084368@163.com>
2021-02-03 22:55:12 +0000

    2.0

--- Catkin/rviz_ws/build/braccio_arduino_ros_rviz/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lcn/Robot-Operating-System/Catkin/rviz_ws/src/braccio_arduino_ros_rviz
-# Build directory: /home/lcn/Robot-Operating-System/Catkin/rviz_ws/build/braccio_arduino_ros_rviz
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
