c2e0e34ef64325d845fcf6ed6fdc09bb06a09cdd
LCN-Ubuntu <18252084368@163.com>
2021-02-03 22:15:07 +0000

    V2.0

---
+++ Catkin/rviz_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/lcn/Robot-Operating-System/Catkin/rviz_ws/src
+# Build directory: /home/lcn/Robot-Operating-System/Catkin/rviz_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("braccio_arduino_ros_rviz")
---
+++ Catkin/rviz_ws/build/braccio_arduino_ros_rviz/CMakeFiles/parse_and_publish.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lcn/Robot-Operating-System/Catkin/rviz_ws/devel/lib/braccio_arduino_ros_rviz/parse_and_publish"
+  "/home/lcn/Robot-Operating-System/Catkin/rviz_ws/devel/lib/braccio_arduino_ros_rviz/parse_and_publish.pdb"
+  "CMakeFiles/parse_and_publish.dir/parse_and_publish/parse_and_publish.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parse_and_publish.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
