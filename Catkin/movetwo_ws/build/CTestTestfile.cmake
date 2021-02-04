3a7a622e0daddb5e3942a48affaae954bb3ddf01
LCN-Ubuntu <18252084368@163.com>
2021-02-04 23:35:08 +0000

    2.2

---
+++ Catkin/movetwo_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/lcn/Robot-Operating-System/Catkin/movetwo_ws/src
+# Build directory: /home/lcn/Robot-Operating-System/Catkin/movetwo_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros_braccio_moveit")
---
+++ Catkin/movetwo_ws/build/ros_braccio_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lcn/Robot-Operating-System/Catkin/movetwo_ws/src/ros_braccio_moveit
+# Build directory: /home/lcn/Robot-Operating-System/Catkin/movetwo_ws/build/ros_braccio_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
